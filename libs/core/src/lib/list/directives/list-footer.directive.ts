c3c491173ad203d5f983a7d56885d51bf3847dab
dmitry-stepanenko <stepanenkodmitri@gmail.com>
2021-12-03 17:34:42 +0200

    fix(core): standard list a11y

--- libs/core/src/lib/list/directives/list-footer.directive.ts
+++ libs/core/src/lib/list/directives/list-footer.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostBinding } from '@angular/core';
+import { Directive, HostBinding, Input } from '@angular/core';
 
 @Directive({
     selector: '[fdListFooter], [fd-list-footer]'
@@ -7,4 +7,9 @@ export class ListFooterDirective {
     /** @hidden */
     @HostBinding('class.fd-list__footer')
     fdListFooterClass = true;
+
+    /** Whether to apply "aria-hidden" attribute. */
+    @Input()
+    @HostBinding('attr.aria-hidden')
+    ariaHidden = true;
 }
